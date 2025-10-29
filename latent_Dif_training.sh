e83b9de8cb6ac27abfe01da04a7d3e66a76fb719
OberonBW <o.buckingham-west@surrey.ac.uk>
2025-10-29 12:25:00 +0000

    update config to load fine tuned decoder

--- latent_Dif_training.sh
+++ latent_Dif_training.sh
@@ -7,6 +7,7 @@ python -m torch.distributed.run --standalone --nnodes=1 --nproc_per_node=2 \
     src/train.py \
     --config /mnt/hitchcock/scratch/oberon/RAE/configs/stage2/training/ImageNet256/DiTDH-S_DINOv2-B_UI.yaml \
     --data-path /mnt/hitchcock/scratch/oberon/action_window_segments/dataset/training_dataset_segments_10tutorials \
-    --results-dir results/stage2 \
-    --precision bf16 \
+    --results-dir results/stage2_test \
+    --image-size 256 \
+    --precision fp32 \
     --wandb
